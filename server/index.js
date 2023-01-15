3fe7fae7cf0a87a3c7789a3758c29fa763bcdc4c
Christopher Steffes <c.steffes@live.ca>
2023-01-15 14:51:05 -0600

    init commit

--- server/index.js
+++ server/index.js
@@ -1,28 +1,30 @@
+const http = require("http").createServer();
 
-const http = require('http').createServer();
-
-const io = require('socket.io')(http, {
-    cors: { origin: "*" }
+const io = require("socket.io")(http, {
+  cors: { origin: "*" },
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-    socket.on('message', (message) =>     {
-        console.log(message);
-        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
-    });
+  socket.on("message", (message) => {
+    console.log(`${socket.id.substr(0, 4)} said ${message}`);
+    io.emit("message", `${socket.id.substr(0, 4)} said ${message}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(socket.id.substr(0, 4) + " disconnected"); // undefined
+  });
 });
 
-http.listen(8080, () => console.log('listening on http://localhost:8080') );
-
+http.listen(8080, () => console.log("listening on http://10.0.0.11:8080"));
 
 // Regular Websockets
 
 // const WebSocket = require('ws')
 // const server = new WebSocket.Server({ port: '8080' })
 
-// server.on('connection', socket => { 
+// server.on('connection', socket => {
 
 //   socket.on('message', message => {
 
@@ -31,6 +33,3 @@ http.listen(8080, () => console.log('listening on http://localhost:8080') );
 //   });
 
 // });
-
-
- 
